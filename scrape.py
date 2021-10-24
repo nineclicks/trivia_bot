8a59d917e7e26edf35286d87e7524f880eb32cec
Nick Gardner <ngardn01@calpoly.edu>
2021-10-23 20:00:46 -0700

    update scrape script

--- scrape.py
+++ scrape.py
@@ -1,3 +1,4 @@
+import sys
 import re
 import requests
 import html
@@ -9,13 +10,15 @@ REG_EPS = r'\"(https:\/\/www\.j-archive\.com\/showgame\.php\?game_id=\d+)\"'
 REG_CAT_COMMENT = r'\(.+?:\s+(.*)\)'
 REG_HTML_TAGS = r'<[^>]+>'
 REG_SHOW_NUM = r'<div id=\"game_title\"><h1>Show #(\d+).*?(\d{4})<\/h1>'
+REG_SEASON_NUMBERS = r'<a href=\"showseason\.php\?season=(\d+)\"'
 
-DB_PATH = 'trivia.db'
+SEASON_LIST_URL = 'https://j-archive.com/listseasons.php'
+SEASON_URL = 'https://www.j-archive.com/showseason.php?season='
 
 sample_page = 'https://www.j-archive.com/showgame.php?game_id=7094'
 
-def build_tables():
-    con = sqlite3.connect(DB_PATH)
+def build_tables(db_path):
+    con = sqlite3.connect(db_path)
     cur = con.cursor()
     cur.execute('''CREATE TABLE IF NOT EXISTS category
                (id INTEGER NOT NULL PRIMARY KEY, show_number number, show_year number, title text, comment text)''')
@@ -26,21 +29,21 @@ def build_tables():
     con.commit()
     con.close()
 
-def parse_page(url):
-    con = sqlite3.connect(DB_PATH)
+def parse_page(url, db_path):
+    con = sqlite3.connect(db_path)
     cur = con.cursor()
     cats = []
     questions = []
     scores = [200, 400, 600, 800, 1000]
+
     page_content = requests.get(url).content.decode('utf-8')
     show_info = re.search(REG_SHOW_NUM, page_content)
     show_num = int(show_info[1])
     show_year = int(show_info[2])
-    
+
     cur.execute('''SELECT DISTINCT show_number FROM category WHERE show_number = ?''', (show_num, ))
     if len(cur.fetchall()) > 0:
-        print('already have')
-        return
+        raise IndexError('Already have this episode')
 
     matches = re.findall(REG_CATEGORIES, page_content)[:12]
     if len(matches) != 12:
@@ -96,7 +99,8 @@ def parse_page(url):
 def clean_string(s):
     return re.sub(REG_HTML_TAGS, '', html.unescape(s)).replace('\\', '')
 
-def scan_season(url):
+def scan_season(number, db_path):
+    url = SEASON_URL + str(number)
     print(url)
     page_content = requests.get(url).content.decode('utf-8')
     matches = re.finditer(REG_EPS, page_content)
@@ -105,13 +109,36 @@ def scan_season(url):
         ep_url = match[1]
         print(ep_url)
         try:
-            parse_page(ep_url)
-        except Exception:
-            print('skip')
-        sleep(.5)
+            parse_page(ep_url, db_path)
+            sleep(.5)
+        except Exception as ex:
+            print(str(ex))
 
+def get_seasons():
+    page_content = requests.get(SEASON_LIST_URL).content.decode('utf-8')
+    seasons = [int(s) for s in re.findall(REG_SEASON_NUMBERS, page_content)]
+    return seasons
 
-build_tables()
-#parse_page('https://www.j-archive.com/showgame.php?game_id=6955')
-for i in range(16, 11, -1):
-    scan_season('https://www.j-archive.com/showseason.php?season={}'.format(i))
+db_path = sys.argv[1]
+commands = sys.argv[2:]
+
+if len(commands) == 0:
+    exit(
+            'usage:\n'
+            f'{sys.argv[0]} latest\n'
+            f'{sys.argv[0]} season x\n'
+            )
+
+if commands[0] == 'latest':
+    season = max(get_seasons())
+
+elif commands[0].startswith('season'):
+    if len(commands[0]) > len('season'):
+        season = int(commands[0][len('season'):])
+
+    else:
+        season = int(commands[1])
+
+build_tables(sys.argv[1])
+scan_season(season, sys.argv[1])
+
